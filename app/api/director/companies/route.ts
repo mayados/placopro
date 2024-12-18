61e261a686ec5e573841d6117290806297c7811e
mayados <mariannedos.cours@gmail.com>
2024-12-18 11:47:43 +0100

    route : create code to get companies

---
+++ app/api/director/companies/route.ts
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+
+    try{
+        const companiesFound = await db.company.findMany({});
+        const companies = companiesFound.map((company: CompanyType) => ({
+            id: company.id,
+            name: company.name,
+            slug: company.slug,
+            type: company.type,
+            phone: company.phone,
+            mail: company.mail,
+            capital: company.capital,
+            rcs: company.rcs,
+            siret: company.rcs,
+            ape: company.ape,
+            intraCommunityVat: company.intraCommunityVat,
+            road: company.road,
+            addressNumber: company.addressNumber,
+            postalCode: company.postalCode,
+            city: company.city,
+            additionnalAddress: company.additionnalAddress,
+            insuranceName: company.decennialInsuranceName,
+            insuranceContractNumber: company.insuranceContractNumber,
+            insuranceCoveredZone: company.aeraCoveredByInsurance,
+          }));
+
+        return NextResponse.json({
+            companies: companies,
+        })
+
+    } catch (error) {
+        console.log("[COMPANIES]", error)
+
+        return new NextResponse("Internal error, {status: 500}")
+    }
+
+}
