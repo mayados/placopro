cffced009539eccd99663c72a40163afae5a748a
mayados <mariannedos.cours@gmail.com>
2024-12-24 11:34:17 +0100

    add code : sort worksites by status with headlessui Tabs

--- app/director/workSites/page.tsx
+++ app/director/workSites/page.tsx
@@ -5,21 +5,42 @@ import { Trash2 } from 'lucide-react';
 import Button from "@/components/Button";
 import { formatDate } from '@/lib/utils'
 // import toast, { Toaster } from 'react-hot-toast';
-import { Dialog, DialogTitle, DialogPanel, Description } from '@headlessui/react';
+import { Dialog, DialogTitle, DialogPanel, Description, Tab, TabGroup ,TabList, TabPanel, TabPanels } from '@headlessui/react';
 import Link from "next/link";
 
 const WorkSites = () =>{
 
-    const [WorkSites, setWorkSites] = useState<WorkSiteType[]>([])
+    // a const for each workSite status
+    const [workSites, setWorkSites] = useState<WorkSiteForListType[]>([])
+    const [commingWorkSites, setCommingWorkSites] = useState<WorkSiteForListType[]>([])
+    const [inProgressWorkSites, setInProgressWorkSites] = useState<WorkSiteForListType[]>([])
+    const [completedWorkSites, setCompletedWorkSites] = useState<WorkSiteForListType[]>([])
+    // const to get total of workSites by status
+    const [totalWorkSites, setTotalWorkSites] = useState<number>(0)
+    const [totalCommingWorkSites, setTotalCommingWorkSites] = useState<number>(0)
+    const [totalInProgressWorkSites, setTotalInProgressWorkSites] = useState<number>(0)
+    const [totalCompletedWorkSites, setTotalCompletedWorkSites] = useState<number>(0)
+    // const to set a workSite if it's selected to be deleted
     const [workSiteToDelete, setWorkSiteToDelete] = useState<string | null>(null); 
+    // const for the modal
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const fetchWorkSites = async () => {
           const response = await fetch(`/api/director/workSites`)
-          const data =  await response.json()
+          const data: WorkSiteWithTotalsAndStatus =  await response.json()
           console.log("données reçues après le fetch : "+data)
-          setWorkSites(data)
+          console.log("exemple d'un worksite reçu : "+data['workSites'][0])
+          // We hydrate each const with the datas
+          setWorkSites(data['workSites'])
+          setCommingWorkSites(data['commingWorkSites'])
+          setInProgressWorkSites(data['inProgressWorkSites'])
+          setCompletedWorkSites(data['completedWorkSites'])
+    
+          setTotalWorkSites(data['totalWorkSites'] || 0)
+          setTotalCommingWorkSites(data['totalCommingWorkSites'] || 0)
+          setTotalInProgressWorkSites(data['totalInProgressWorkSites'] || 0)
+          setTotalCompletedWorkSites(data['totalCompletedWorkSites'] || 0)
     
         }
     
@@ -58,48 +79,186 @@ const WorkSites = () =>{
         {/* <div><Toaster/></div> */}
 
         <section className="border-2 border-green-800 flex-[8]">
-            <h1 className="text-3xl text-white text-center">Entreprises</h1>
+            <h1 className="text-3xl text-white text-center">Chantiers</h1>
             <Link href={`/director/workSites/create`}>Créer un chantier</Link>
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>Chantier</th>
-                        <th>Date de début</th>
-                        <th>Ville</th>
-                        <th>Afficher</th>
-                        <th>Modifier</th>
-                        <th>Supprimer</th>
-                    </tr>  
-                </thead>
-                <tbody>
-                {
-                    WorkSites.map((workSite) => {
-                        const workSiteId = workSite.id;
-                    
-                    return (
-                        <tr key={workSite.id}>
-                            <td>{workSite.title}</td>
-                            <td>{formatDate(workSite.beginsThe)}</td>
-                            <td>{workSite.city}</td>
-                            <td>
-                            <Link href={`/director/workSites/${workSite?.slug}`}>
-                                Consulter les détails
-                            </Link>
-                          </td>
-                          <td>
-                            <Link href={`/director/workSites/${workSite?.slug}/update`}>
-                                Modifier
-                            </Link>
-                          </td>
-                            <td>
-                                <Button label="Remove" icon={Trash2} type="button" action={() => openDeleteDialog(workSiteId)} specifyBackground="text-red-500" />
-                            </td>
-                        </tr>
-                    );
-                    })
-                }
-                </tbody>
-            </table>   
+            <TabGroup className="flex flex-col items-center lg:block my-3">
+                <TabList className="my-3 flex gap-3">
+                    <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Tous ({totalWorkSites})</Tab>
+                    <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">En cours ({totalInProgressWorkSites})</Tab>
+                    <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">A venir ({totalCommingWorkSites})</Tab>
+                    <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Terminés ({totalCompletedWorkSites})</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel className="flex flex-row gap-5 flex-wrap justify-center lg:justify-between">
+                        <table className="table-auto">
+                            <thead>
+                                <tr>
+                                    <th>Chantier</th>
+                                    <th>Date de début</th>
+                                    <th>Ville</th>
+                                    <th>Afficher</th>
+                                    <th>Modifier</th>
+                                    <th>Supprimer</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                            {
+                                workSites.map((workSite) => {
+                                    const workSiteId = workSite.id;
+                                
+                                return (
+                                    <tr key={workSite.id}>
+                                        <td>{workSite.title}</td>
+                                        <td>{formatDate(workSite.beginsThe)}</td>
+                                        <td>{workSite.city}</td>
+                                        <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}`}>
+                                            Consulter les détails
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}/update`}>
+                                            Modifier
+                                        </Link>
+                                    </td>
+                                        <td>
+                                            <Button label="Remove" icon={Trash2} type="button" action={() => openDeleteDialog(workSiteId)} specifyBackground="text-red-500" />
+                                        </td>
+                                    </tr>
+                                );
+                                })
+                            }
+                            </tbody>
+                        </table>  
+                    </TabPanel>
+                    <TabPanel className="flex flex-row gap-5 flex-wrap justify-center lg:justify-between">
+                        <table className="table-auto">
+                            <thead>
+                                <tr>
+                                    <th>Chantier</th>
+                                    <th>Date de début</th>
+                                    <th>Ville</th>
+                                    <th>Afficher</th>
+                                    <th>Modifier</th>
+                                    <th>Supprimer</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                            {
+                                inProgressWorkSites.map((workSite) => {
+                                    const workSiteId = workSite.id;
+                                
+                                return (
+                                    <tr key={workSite.id}>
+                                        <td>{workSite.title}</td>
+                                        <td>{formatDate(workSite.beginsThe)}</td>
+                                        <td>{workSite.city}</td>
+                                        <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}`}>
+                                            Consulter les détails
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}/update`}>
+                                            Modifier
+                                        </Link>
+                                    </td>
+                                        <td>
+                                            <Button label="Remove" icon={Trash2} type="button" action={() => openDeleteDialog(workSiteId)} specifyBackground="text-red-500" />
+                                        </td>
+                                    </tr>
+                                );
+                                })
+                            }
+                            </tbody>
+                        </table>    
+                    </TabPanel>
+                    <TabPanel className="flex flex-row gap-5 flex-wrap justify-center lg:justify-between">
+                        <table className="table-auto">
+                            <thead>
+                                <tr>
+                                    <th>Chantier</th>
+                                    <th>Date de début</th>
+                                    <th>Ville</th>
+                                    <th>Afficher</th>
+                                    <th>Modifier</th>
+                                    <th>Supprimer</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                            {
+                                commingWorkSites.map((workSite) => {
+                                    const workSiteId = workSite.id;
+                                
+                                return (
+                                    <tr key={workSite.id}>
+                                        <td>{workSite.title}</td>
+                                        <td>{formatDate(workSite.beginsThe)}</td>
+                                        <td>{workSite.city}</td>
+                                        <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}`}>
+                                            Consulter les détails
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}/update`}>
+                                            Modifier
+                                        </Link>
+                                    </td>
+                                        <td>
+                                            <Button label="Remove" icon={Trash2} type="button" action={() => openDeleteDialog(workSiteId)} specifyBackground="text-red-500" />
+                                        </td>
+                                    </tr>
+                                );
+                                })
+                            }
+                            </tbody>
+                        </table>   
+                    </TabPanel>
+                    <TabPanel className="flex flex-row gap-5 flex-wrap justify-center lg:justify-between">
+                        <table className="table-auto">
+                            <thead>
+                                <tr>
+                                    <th>Chantier</th>
+                                    <th>Date de début</th>
+                                    <th>Ville</th>
+                                    <th>Afficher</th>
+                                    <th>Modifier</th>
+                                    <th>Supprimer</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                            {
+                                completedWorkSites.map((workSite) => {
+                                    const workSiteId = workSite.id;
+                                
+                                return (
+                                    <tr key={workSite.id}>
+                                        <td>{workSite.title}</td>
+                                        <td>{formatDate(workSite.beginsThe)}</td>
+                                        <td>{workSite.city}</td>
+                                        <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}`}>
+                                            Consulter les détails
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <Link href={`/director/workSites/${workSite?.slug}/update`}>
+                                            Modifier
+                                        </Link>
+                                    </td>
+                                        <td>
+                                            <Button label="Remove" icon={Trash2} type="button" action={() => openDeleteDialog(workSiteId)} specifyBackground="text-red-500" />
+                                        </td>
+                                    </tr>
+                                );
+                                })
+                            }
+                            </tbody>
+                        </table>     
+                    </TabPanel>
+                </TabPanels>
+            </TabGroup>  
         </section>  
         {/* Delete workSite Dialog */}
         {isOpen && workSiteToDelete && (
