325cf309622bbbca42162ab46ec872238a672790
mayados <mariannedos.cours@gmail.com>
2025-01-14 08:46:15 +0100

    create API route : to get unit values

---
+++ app/api/director/units/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+
+    try{
+        const units = await db.unit.findMany(
+            {
+                select: {
+                    id: true,
+                    label: true
+                  },
+            }
+        );
+
+        return NextResponse.json({
+            units: units,
+        })
+
+    } catch (error) {
+        console.log("[UNITS]", error)
+
+        return new NextResponse("Internal error, {status: 500}")
+    }
+
+}
