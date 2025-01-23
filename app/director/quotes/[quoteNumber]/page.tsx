6970934f75c034acdb6fff18478d713505713a79
mayados <mariannedos.cours@gmail.com>
2025-01-23 09:47:26 +0100

    add const, functions, ul, section and form : all these elements linked to the update of the fields status, isSignedByClient and signatureDate for quotes which don't have draft status

--- app/director/quotes/[quoteNumber]/page.tsx
+++ app/director/quotes/[quoteNumber]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, use } from "react";
 import { formatDate } from '@/lib/utils'
 import DownloadPDF from "@/components/DownloadPDF";
+import { Field, Input, Label, Legend, Radio, RadioGroup, Select } from "@headlessui/react";
 
 // import toast, { Toaster } from 'react-hot-toast';
 // import { useRouter } from "next/navigation";
@@ -13,6 +14,14 @@ const Quote = ({ params }: { params: Promise<{ quoteNumber: string }>}) => {
     const [company, setCompany] = useState<CompanyType | null>(null);
     const [vatAmountTravelCost, setVatAmountTravelCost] = useState<number>(0)
     const [priceTTCTravelCost, setPriceTTCTravelCost] = useState<number>(0)
+    const quoteStatusChoices = ["Prêt à l'envoi","Envoyé","Accepté","Refusé"];
+    const isSignedByClientChoices = ["Oui","Non"];
+    const [formValues, setFormValues] = useState<FormValuesUpdateNotDraftQuote>({
+        id: null,
+        status: null,
+        isSignedByClient: null,
+        signatureDate: null,
+    })
 
     
         useEffect(() => {
@@ -25,6 +34,10 @@ const Quote = ({ params }: { params: Promise<{ quoteNumber: string }>}) => {
             const response = await fetch(`/api/director/quotes/${quoteNumber}`);
             const data: QuoteTypeSingle = await response.json();
             setQuote(data.quote); 
+            setFormValues({
+                ...formValues,
+                id: data.quote.id,
+            });
             if(data.quote.travelCosts){
                 // travelCosts cost => vatAmount and priceTTC
                 setVatAmountTravelCost((data.quote.travelCosts) * (20/100))  
@@ -46,12 +59,117 @@ const Quote = ({ params }: { params: Promise<{ quoteNumber: string }>}) => {
           fetchCompany()
         }, [params]);
 
+        const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+            console.log("évènement reçu : "+e)
+            const { name, value } = e.target;
+            console.log("select :"+name+" valeur : "+value)
+            setFormValues({
+                ...formValues,
+                [name]: value,
+            });
+                  
+        };
+    
+        //   Retrieve datas from the radio buttons. Because they are in a RadioGroup, we can't retrieve the value just thanks to an event, we have to get the name (of the group) + the value selected
+        const handleRadioChange = (name: string, value: string) => {
+            setFormValues((formValues) => ({
+              ...formValues,
+              [name]: value,
+            }));
+          };
+
+        const handleSubmit = async () => {
+        try{
+    
+            const response = await fetch(`/api/director/quotes/update/${quote?.number}`, {
+                method: "PUT",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formValues),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log("data renvoyés : "+data.updatedQuote)
+                const updatedQuote = data.updatedQuote;
+                console.log("voici le devis mis à jour : "+updatedQuote.number)
+                setQuote(updatedQuote)
+            }
+            }catch (error) {
+                console.error("Erreur lors de la mise à jour du devis :", error);
+            }
+    
+        };
+    
+
         if (!quote) return <div>Loading...</div>;
       
     return (
         <>
             {/* <div><Toaster/></div> */}
             <h1 className="text-3xl text-white ml-3 text-center">Devis {quote?.number}</h1>
+            
+            <ul>
+                <li>Statut : {quote.status}</li>
+                <li>Signé par le client ? {quote.isSignedByClient ? "Oui" : 'Non'}</li>
+                <li>Date de signature : {quote.signatureDate ? formatDate(quote.signatureDate) : '/'}</li>
+
+            </ul>
+            {/* If the quote's status is different from draft, we can display the form */}
+                {quote.status !== "draft" && (
+                    <section>
+                        <h2>Modifier les informations</h2>
+                        <form 
+                            autoComplete="off"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                handleSubmit();
+                            }}
+                        >
+                            <div>
+                                <Select
+                                    name="status"
+                                    onChange={handleInputChange}
+                                    value={formValues.status || ""}
+                                    className="w-full rounded-md bg-gray-700 text-white pl-3"
+                                >
+                                <option value="">Statut</option>
+                                    {quoteStatusChoices.map((status) => (
+                                        <option key={status} value={status}>{status}</option>
+                                    ))}
+                                </Select>
+                            </div>
+                            <div>
+                                <Field>
+                                    <Legend>Le devis a t-il été signé par le client ?</Legend>
+                                    <RadioGroup 
+                                        name="isSignedByClient"
+                                        onChange={(value)=> handleRadioChange("isSignedByClient",value)}
+
+                                    >
+                                        {isSignedByClientChoices.map((choice) => (
+                                            <Field key={choice} className="flex gap-2 items-center">
+                                                <Radio value={choice} className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-pink-600" />
+                                                <Label>{choice}</Label>
+                                            </Field>
+                                        ))}
+                                    </RadioGroup>
+                                </Field>
+                            </div>
+                            <div>
+                                <label htmlFor="signatureDate">Date de signature</label>
+                                <Field className="w-full">
+                                    <Input type="date" name="signatureDate" className="w-full h-[2rem] rounded-md bg-gray-700 text-white pl-3" 
+                                        onChange={handleInputChange}
+                                    >
+                                    </Input>
+                                </Field>
+                            </div>
+                            <button type="submit">Modifier</button>
+                        </form>                        
+                    </section>
+
+                )}
             <DownloadPDF quote={quote} company={company} vatAmountTravelCost={vatAmountTravelCost} priceTTCTravelCost={priceTTCTravelCost} />
             {/* <div><Toaster /></div> */}
             <section>
