f13cb693404a3942b682ef6f904fd960ed078b7a
mayados <mariannedos.pro@gmail.com>
2025-03-12 16:09:28 +0100

    add links to api route of bill in route

---
+++ app/api/bills/[billNumber]/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PUT as updateDraftBill } from "@/app/api/bills/[billNumber]/update-draft";
+import { PUT as updateClassicBill } from "@/app/api/bills/[billNumber]/update-classic";
+import { GET as getBill } from "@/app/api/bills/[billNumber]/get";
+import { DELETE as deleteQuote } from "@/app/api/quote/[quoteNumber]/delete";
+
+export async function GET(req: NextRequest, { params }: { params: { billNumber: string } }) {
+  return getBill(req, { params });
+}
+
+export async function PUT(req: NextRequest, { params }: { params: { quoteNumber: string } }) {
+  // Decide which type of PUT to call
+  if (req.headers.get('X-Update-Type') === 'draft') {
+    // Update draft (=all the fields)
+    return updateDraftBill(req);
+  }
+  
+  // Classic update by default
+  return updateClassicBill(req);
+}
