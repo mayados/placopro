403395f121819705b7a3cad3ff437f52d2f54133
mayados <mariannedos.pro@gmail.com>
2025-03-30 15:04:37 +0200

    use pathname for requests (delete params)

--- app/api/bills/[billNumber]/route.ts
+++ app/api/bills/[billNumber]/route.ts
@@ -5,22 +5,49 @@ import { PUT as updateClassicBill } from "@/app/api/bills/[billNumber]/update-cl
 import { GET as getBill } from "@/app/api/bills/[billNumber]/get";
 import { DELETE as deleteQuote } from "@/app/api/quote/[quoteNumber]/delete";
 
-export async function GET(req: NextRequest, { params }: { params: { billNumber: string } }) {
-  return getBill(req, { params });
+export async function GET(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const billNumber = pathname.split("/").pop(); // Récupère billNumber depuis l'URL
+
+  if (!billNumber) {
+    return NextResponse.json({ error: "billNumber is required" }, { status: 400 });
+  }
+
+  return getBill(req, { params: { billNumber } });
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { quoteNumber: string } }) {
-  // Decide which type of PUT to call
-  if (req.headers.get('X-Update-Type') === 'draft') {
-    // Update draft (=all the fields)
+// export async function PUT(req: NextRequest, { params }: { params: { quoteNumber: string } }) {
+//   // Decide which type of PUT to call
+//   if (req.headers.get('X-Update-Type') === 'draft') {
+//     // Update draft (=all the fields)
+//     return updateDraftBill(req);
+//   }
+
+//   if (req.headers.get('X-Update-Type') === 'draft-deposit') {
+//     // Update draft (=all the fields)
+//     return updateDepositDraftBill(req);
+//   }
+  
+//   // Classic update by default
+//   return updateClassicBill(req);
+// }
+
+export async function PUT(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const quoteNumber = pathname.split("/").pop();
+
+  if (!quoteNumber) {
+    return NextResponse.json({ error: "billNumber is required" }, { status: 400 });
+  }
+
+  // Vérifie le header et route vers la bonne fonction
+  if (req.headers.get("X-Update-Type") === "draft") {
     return updateDraftBill(req);
   }
 
-  if (req.headers.get('X-Update-Type') === 'draft-deposit') {
-    // Update draft (=all the fields)
+  if (req.headers.get("X-Update-Type") === "draft-deposit") {
     return updateDepositDraftBill(req);
   }
-  
-  // Classic update by default
+
   return updateClassicBill(req);
 }
