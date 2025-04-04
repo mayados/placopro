736b6d2c567dca48e916c1742661b5b98ac78669
mayados <mariannedos.pro@gmail.com>
2025-04-04 13:51:43 +0200

    create zod validation schema for client, company, creditNote and planning

--- validation/clientValidation.ts
+++ validation/clientValidation.ts
@@ -30,4 +30,18 @@ export const createClientSchema = z.object({
 
 })
 
+export const updateClientSchema = z.object({
+    name: z.string().min(2, "Le nom est requis"),
+    firstname: z.string().min(2, "Le prénom est requis"),
+    phone: z.string().min(10, "Le numéro de téléphone est requis"),
+    road: z.string().min(5, "La rue est requise"),
+    addressNumber: z.string().min(1, "Le numéro de bâtiment est requis"),
+    postalCode: z.string().min(5, "Le code postal est requis"),
+    city: z.string().min(1, "La ville est requise"),
+    additionalAddress: z.string().min(1, "Le complément d'adresse est requis"),
+    mail: z.string().min(1, "L'email est requis").email("Format d'email invalide"),
+    prospectNumber: z.string().nullable().optional(),
+
+})
+
 
---
+++ validation/creditNoteValidation.ts
@@ -0,0 +1,17 @@
+// /validation/billValidation.ts
+import { z } from 'zod';
+import { CreditNoteReasonEnum } from '@prisma/client';
+
+export const createCreditNoteSchema = z.object({
+    amount: z.number().min(1,"Le montant est requis"),
+    reason: z.nativeEnum(CreditNoteReasonEnum),
+
+})
+
+export const updateCreditNoteSchema = z.object({
+    isSettled: z.boolean().nullable(),
+    settlementType: z.nativeEnum(CreditNoteSettlementTypeEnum).nullable(),
+
+})
+
+
