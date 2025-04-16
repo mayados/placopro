548e4284146dc4c8d5f5809b4eefe07d9cd67539
mayados <mariannedos.pro@gmail.com>
2025-04-16 17:26:54 +0200

    fix : await params

--- app/director/workSites/[workSiteSlug]/update/page.tsx
+++ app/director/workSites/[workSiteSlug]/update/page.tsx
@@ -4,13 +4,15 @@ import { headers } from 'next/headers'
 import UpdateWorkSite from '@/components/UpdateWorkSite' 
 
 export default async function Page({ params }: { params: { workSiteSlug: string } }) {
+  const { workSiteSlug } = await params;
+
   const h = await headers()
   const csrfToken = h.get('X-CSRF-Token') || 'missing'
 
   // Once we get the csrf token and billNumber, we can give it to the component
   return (
     <UpdateWorkSite
-      workSiteSlug={params.workSiteSlug}
+      workSiteSlug={workSiteSlug}
       csrfToken={csrfToken}
     />
   )
