d4eaa957800e7a0ed00bf44e96178e8d20460918
mayados <mariannedos.pro@gmail.com>
2025-04-21 22:32:01 +0200

    add : import CreditNoteSettlementTypeEnum

--- validation/creditNoteValidation.ts
+++ validation/creditNoteValidation.ts
@@ -1,6 +1,7 @@
 // /validation/billValidation.ts
 import { z } from 'zod';
 import { CreditNoteReasonEnum } from '@prisma/client';
+import { CreditNoteSettlementTypeEnum } from '@prisma/client';
 
 export const createCreditNoteSchema = z.object({
     amount: z.number().min(1,"Le montant est requis"),
