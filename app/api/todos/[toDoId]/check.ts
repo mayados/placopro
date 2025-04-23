ffaa30f466547fbb94d61fdb3eb8b4c25c19c1ff
mayados <mariannedos.pro@gmail.com>
2025-04-23 22:04:46 +0200

    modify : add condition to treat the case of check and uncheck to do

--- app/api/todos/[toDoId]/check.ts
+++ app/api/todos/[toDoId]/check.ts
@@ -5,11 +5,28 @@ export async function PATCH(req: NextRequest) {
   const data = await req.json();
   const toDoId = data.toDoId
   try {
-    const updatedToDo = await db.toDo.update({
+
+    const currentToDo = await db.toDo.findUnique({
+      where: {id: toDoId}
+    });
+
+    let updatedToDo;
+
+    if(currentToDo?.isChecked === false){
+      console.log("le to do actuel n'est pas checked donc on doit le checked")
+       updatedToDo = await db.toDo.update({
+          where: { id: toDoId },
+          data: { isChecked: true },
+        });      
+    }else{
+      console.log("le to do actuel est checked. Donc on doit le unchecked")
+
+      updatedToDo = await db.toDo.update({
         where: { id: toDoId },
-        data: { isChecked: true },
-      });
-  
+        data: { isChecked: false },
+      });        
+    }
+
       return NextResponse.json({ success: true, toDo: updatedToDo });
 
   } catch (error) {
