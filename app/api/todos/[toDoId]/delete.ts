1f681542d00724072dae6c94626b0d76fc24c892
mayados <mariannedos.pro@gmail.com>
2025-04-23 22:06:22 +0200

    add : route to delete a to do

---
+++ app/api/todos/[toDoId]/delete.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+
+// Asynchrone : waits for a promise
+export async function DELETE(req: NextRequest, {params}: {params: {toDoId: string}})
+{
+    const resolvedParams = await params;
+
+    const toDoId = resolvedParams.toDoId;
+
+    try{
+
+        await db.toDo.delete({
+            where: {
+                id: toDoId
+            }            
+        })
+
+        return new NextResponse(JSON.stringify({ success: true, message: 'To do deleted with success' }), {
+            status: 200,
+        });
+
+    } catch (error) {
+        // servor side : SSR
+        console.error("Erreur détaillée :", error instanceof Error ? error.message : error);
+
+        return new NextResponse("Internal error, {status: 500}")
+    }
+
+
+}
