f5dd5f6745895e53807eff877df577435ce6343b
mayados <mariannedos.pro@gmail.com>
2025-04-23 08:37:02 +0200

    add : create.ts file to create a classic to do

---
+++ app/api/todos/create.ts
@@ -0,0 +1,61 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { currentUser } from '@clerk/nextjs/server'
+import { createClassicToDoSchema } from "@/validation/toDoValidation";
+import { sanitizeData } from "@/lib/sanitize"; 
+
+
+// Asynchrone : waits for a promise
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+
+    // currentUser() is a founction from Clerk which allows to retrieve the current User
+    const user = await currentUser()
+
+
+    try {
+        
+        if (!user) {
+            return NextResponse.json({ 
+                success: false, 
+                message: "Utilisateur non authentifié." 
+            }, { status: 401 });
+        }
+
+        // Validation avec Zod
+        const parsedData = createClassicToDoSchema.safeParse(data);
+        if (!parsedData.success) {
+            console.error("Validation Zod échouée :", parsedData.error.format());
+                
+            return NextResponse.json({ success: false, message: parsedData.error.errors }, { status: 400 });
+        }
+                        
+        // Sanitizing datas
+        const sanitizedData = sanitizeData(parsedData.data);
+        console.log("Données nettoyées :", JSON.stringify(sanitizedData));
+
+
+        // We create the to do thanks to te datas retrieved
+        const toDo = await db.toDo.create({
+            data: {
+                task: sanitizedData.task,
+                description: sanitizedData.description,
+                authorClerkId: user.id,
+                isChecked: false,
+                isArchived: false
+            },
+        });
+
+
+        console.log("To do créé avec succès.");
+        // Toujours retourner la réponse après la création
+        return NextResponse.json({ success: true, data: toDo });
+
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            error: error
+        }, { status: 500 });
+    }
+}
