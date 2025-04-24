50b6fe5a0381a2e2d7acf0f50d1796a97f73293d
mayados <mariannedos.pro@gmail.com>
2025-04-24 11:36:12 +0200

    add : updateClassicToDoSchema and createAssignedToDoSchema

--- validation/toDoValidation.ts
+++ validation/toDoValidation.ts
@@ -6,4 +6,14 @@ export const createClassicToDoSchema = z.object({
     description: z.string().nullable(),
 })
 
+export const updateClassicToDoSchema = z.object({
+    task: z.string().min(3).optional(),
+    description: z.string().nullable().optional()
+})
 
+
+export const createAssignedToDoSchema = z.object({
+    task: z.string().min(3, "La tâche est requise avec un minimum de 3 caractères"),
+    description: z.string().nullable(),
+    assignedToClerkId: z.string().min(3, "Le ou la secrétaire est requis(e)"),
+})
