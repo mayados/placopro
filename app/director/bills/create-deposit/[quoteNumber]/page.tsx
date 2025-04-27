828b7fd8539fd8fb92d52335c864b18d02ef928c
mayados <mariannedos.pro@gmail.com>
2025-04-27 11:38:08 +0200

    add : await for quoyteNumberParam

--- app/director/bills/create-deposit/[quoteNumber]/page.tsx
+++ app/director/bills/create-deposit/[quoteNumber]/page.tsx
@@ -4,13 +4,15 @@ import { headers } from 'next/headers'
 import CreationDepositBillFromQuote from '@/components/CreationDepositBillFromQuote' 
 
 export default async function Page({ params }: { params: { quoteNumber: string } }) {
+  
+  const quoteNumberParam = await Promise.resolve(params.quoteNumber);
   const h = await headers()
   const csrfToken = h.get('X-CSRF-Token') || 'missing'
 
   // Once we get the csrf token and billNumber, we can give it to the component
   return (
     <CreationDepositBillFromQuote
-      quoteNumber={params.quoteNumber}
+      quoteNumber={quoteNumberParam}
       csrfToken={csrfToken}
     />
   )
