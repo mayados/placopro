a78365067152e62ab8e4c377a31392bbc4ca884b
mayados <mariannedos.pro@gmail.com>
2025-04-30 13:45:44 +0200

    add : condition to redirect to correct api route if the headers contains x post type

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,7 @@
 // This file allows to handle different actions based on the http method retrieved
 import { NextRequest, NextResponse } from "next/server";
 import { POST as createUser } from "@/app/api/users/create"; 
+import { POST as setPassword } from "@/app/api/users/set-password"; 
 import { GET as getUsers} from "@/app/api/users/list"; 
 
 export async function GET(req: NextRequest) {
@@ -9,6 +10,10 @@ export async function GET(req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
+
+      if (req.headers.get('X-post-type') === 'set-password') {
+        return setPassword(req);
+      }
   return createUser(req);  
 }
 
