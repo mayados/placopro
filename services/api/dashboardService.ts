b0fe1c546bb48501e6531e04a1db3ae50592a5b0
mayados <mariannedos.pro@gmail.com>
2025-05-13 09:44:08 +0200

    add : fetchEmployeeDatas and fetchSecretaryDatas in service dashboardService

--- services/api/dashboardService.ts
+++ services/api/dashboardService.ts
@@ -19,3 +19,43 @@ export const fetchDirectorDatas = async (cookie: string): Promise<DirectorDashbo
       throw error;
     }
   };
+
+// Retrieve dashboard datas for secretary
+export const fetchSecretaryDatas = async (cookie: string): Promise<SecretaryDashboardDatas> => {
+    try {
+
+      const response = await fetch(`${baseUrl}/api/dashboards/secretary`,{
+        method: "GET",
+        headers: { cookie },
+
+      });
+      if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
+  
+      const data: SecretaryDashboardDatas = await response.json();
+      console.log("Données reçues après le fetch :", data);
+      return data;
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données du dashboard secrétaire :", error);
+      throw error;
+    }
+  };
+
+// Retrieve dashboard datas for secretary
+export const fetchEmployeeDatas = async (cookie: string): Promise<EmployeeDashboardDatas> => {
+    try {
+
+      const response = await fetch(`${baseUrl}/api/dashboards/employee`,{
+        method: "GET",
+        headers: { cookie },
+
+      });
+      if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
+  
+      const data: EmployeeDashboardDatas = await response.json();
+      console.log("Données reçues après le fetch :", data);
+      return data;
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données du dashboard secrétaire :", error);
+      throw error;
+    }
+  };
