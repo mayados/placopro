4fb941c9829101eda63261714ac8c78cb041ebda
mayados <mariannedos.pro@gmail.com>
2025-05-15 14:37:42 +0200

    test : add again ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import "./globals.css";
-// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   // const isIntranet = pathname.startsWith("/intranet");
 
   return (
-    // <ClerkProvider>
+    <ClerkProvider>
       <html lang="fr" className="h-full">
         <body className="flex flex-col h-full min-h-screen antialiased">
           {/* public Nav only if we are not in /intranet */}
@@ -31,6 +31,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         </body>
       </html>
-    // </ClerkProvider>
+    </ClerkProvider>
   );
 }
