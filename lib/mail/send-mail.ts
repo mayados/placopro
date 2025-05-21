e4e409db093c879a38030f5e64c7eae6cd7f4f2c
mayados <mariannedos.pro@gmail.com>
2025-05-21 17:54:26 +0200

    Fix : problem with sending mail by contact form, corrected by adding the correct env variable

--- lib/mail/send-mail.ts
+++ lib/mail/send-mail.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 465,
-  secure: process.env.SMTP_SECURE === 'true',
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
